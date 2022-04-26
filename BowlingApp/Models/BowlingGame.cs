6022d47bbca6a6469315961b8b8a7149b957b530
CodeyFlex <sej920@hotmail.com>
2022-04-26 16:29:29 +0200

    Add project files.

---
+++ BowlingApp/Models/BowlingGame.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingApp.Models
+{
+    class BowlingGame
+    {
+        private int CurrentPins; //Tilføj max and min values senere
+        private int Points_Strike = 10;
+
+    }
+}
---
+++ BowlingApp/Models/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BowlingApp.Models
+{
+    class Player
+    {
+        private string Name;
+        private int Color; //Kan sikkert laves om til en reel colour betegnelse
+        private int Points;
+    }
+}
